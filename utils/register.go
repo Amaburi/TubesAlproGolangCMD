5d6b353ff12635d26f1c555b68c272ec3dbda56c
Amaburi <drsservant@gmail.com>
2024-05-19 13:47:47 +0700

    change on the register

--- utils/register.go
+++ utils/register.go
@@ -8,7 +8,7 @@ type Users struct {
 }
 
 func Register(userr *Users) bool {
-
+	var n int
 	var pass string
 	fmt.Print("Enter username: ")
 	fmt.Scan(&userr.Username)
@@ -16,7 +16,7 @@ func Register(userr *Users) bool {
 	fmt.Print("Enter password: ")
 	fmt.Scan(&userr.Password)
 
-	for i := 0; i < 5; i++ {
+	for n < 5 {
 		fmt.Print("Renter password: ")
 		fmt.Scan(&pass)
 		if userr.Password == pass {
@@ -24,8 +24,10 @@ func Register(userr *Users) bool {
 			fmt.Println("Registration successful!")
 			return true
 		} else {
-			fmt.Println("Passwords do not match. Please try again. Maximum attempts are 5")
+			n++
+			fmt.Println("Passwords do not match. Please try again. Maximum attempts are 5, Current attempts are", n)
 		}
+
 	}
 	fmt.Println("Registration failed. Maximum attempts reached.")
 	return false
